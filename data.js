3059d9cdd5adeb06a9a3c50cd54086b485f62136
Muhammad Darwis <75767311+darwisabdrauf@users.noreply.github.com>
2024-05-16 16:55:25 +0800

    add initial files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+const myProjectData = [
+    {
+        title: "Mini Restaurant Ordering App",
+        description: "A user-friendly mobile app for restaurant ordering, offering seamless navigation, menu browsing, and payment options.",
+        tags: ["HTML", "CSS", "JavaScript", "UI Design", "GitHub"],
+        url: "https://restaurant-ordering-app-darwisabdrauf.vercel.app/",
+        screenshot: "",
+        id: 1
+    },
+    {
+        title: "Find Your Movie",
+        description: "A responsive website that enables users to search for their favorite movies and save them to their watchlist. This website also allows users to update their watchlist anytime, anywhere.",
+        tags: ["Responsive Web Design", "RESTful API", "Asynchronous JS"],
+        url: "https://findyourmovie-by-darwisabdrauf.netlify.app/",
+        screenshot: "",
+        id: 2
+    },
+    {
+        title: "Twimba (Twitter Clone)",
+        description: "Twimba is a Twitter (now known as X) clone that I developed as part of my learning journey on Scrimba. Inspired by the functionality and design of Twitter, Twimba allows users to post tweets, like tweets, retweet tweets and also see tweet replies.",
+        tags: ["CRUD", "JavaScript", "UI Design", "GitHub"],
+        url: "https://twimba-darwisabdrauf.netlify.app/",
+        screenshot: "",
+        id: 3
+    },
+    {
+        title: "Headphone Product Page",
+        description: "A clean and responsive product landing page inspired by the renowned Corsair headphone brand. Users can see the product features, pricing and use contact for any enquiry about the product",
+        tags: ["HTML", "CSS", "Responsive Design", "Product Page"],
+        url: "",
+        screenshot: "",
+        id: 4
+    },
+    {
+        title: "",
+        description: "",
+        tags: [""],
+        url: "",
+        screenshot: "",
+        id: 5
+    }
+]
---
+++ index.js
@@ -0,0 +1,23 @@
+const myProject = document.getElementById('projects')
+const rightBtn = document.getElementById('right-btn')
+const leftBtn = document.getElementById('left-btn')
+
+//  EVENT LISTENER
+rightBtn.addEventListener("click", () => myProject.scrollBy({ left: 550, behavior: 'smooth' }))
+leftBtn.addEventListener("click", () => myProject.scrollBy({ left: -550, behavior: 'smooth' }))
+
+document.addEventListener("DOMContentLoaded", () => {
+    const project3 = document.querySelector('.project-card:nth-child(3)')
+    const containerWidth = myProject.clientWidth
+    const cardWidth = project3.clientWidth
+
+    // Calculate the scroll position to center Project 3 and Project 4
+    const scrollPosition = project3.offsetLeft - (containerWidth - cardWidth) / 2
+
+    // Scroll the container to the calculated position
+    myProject.scrollTo({
+        left: scrollPosition,
+    })
+})
+
+console.info("Hello there, thanks for visiting my portfolio")
