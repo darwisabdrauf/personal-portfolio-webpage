b5a88032813e6873cc254dfa0c221dcd3ead0cf2
Muhammad Darwis <75767311+darwisabdrauf@users.noreply.github.com>
2024-05-16 20:19:19 +0800

    minor fix & updates
    - updated intro-text in html
    - updated width for intro-text
    - refactored scroll amount for project-card

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const rightBtn = document.getElementById('right-btn')
 const leftBtn = document.getElementById('left-btn')
 
 //  EVENT LISTENER
-rightBtn.addEventListener("click", () => myProject.scrollBy({ left: 550, behavior: 'smooth' }))
-leftBtn.addEventListener("click", () => myProject.scrollBy({ left: -550, behavior: 'smooth' }))
+rightBtn.addEventListener("click", () => myProject.scrollBy({ left: 450, behavior: 'smooth' }))
+leftBtn.addEventListener("click", () => myProject.scrollBy({ left: -450, behavior: 'smooth' }))
 
 document.addEventListener("DOMContentLoaded", () => {
     const project3 = document.querySelector('.project-card:nth-child(2)')
     const containerWidth = myProject.clientWidth
     const cardWidth = project3.clientWidth
 
-    // Calculate the scroll position to be in the center 
+    // Calculate the scroll position to center Project 3 and Project 4
     const scrollPosition = project3.offsetLeft - (containerWidth - cardWidth) / 2
 
     // Scroll the container to the calculated position
